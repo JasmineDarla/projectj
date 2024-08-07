724b08fb0c923304194645dd65039f0d13ed9672
unknown <jasminesony759@gmail.com>
2024-08-07 11:23:45 +0530

    third

--- src/app/page.js
+++ src/app/page.js
@@ -8,8 +8,41 @@ export default function Home() {
   const handleClick=()=>{
     setCnt(cnt+1)
   }
-  return<div>
+  return <div>
     <h1>{cnt}</h1>
     <button onclick={handleClick}>click</button>
   </div>
+   
+   const [result,setResult]=useState('');
+   const [no,setNo]=useState('')
+   const fnChange=(eve)=>{
+     setNo(eve.target.value)
+   }
+   const fnCheckPrime=(num)=>{
+     if (num < 2) { 
+         return false;
+     }
+     for (let i = 2; i <= Math.sqrt(num); i++) {
+         if (num % i === 0) {
+         return false;
+         }
+     }
+     return true;
+   }
+   const fnClick=()=>{
+     const isPrime=fnCheckPrime(no);
+     setResult(isPrime ? 'Prime':'Not Prime')
+   }
+  
+     return <div>
+            <p>
+             <b>No: </b><input type="number" onChange={fnChange} />
+            </p>
+            <p>
+             <button onClick={fnClick}>Check Prime</button>
+            </p>
+            <h1>{result}</h1>
+         </div>
+   
+
 }
