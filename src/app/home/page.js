34564adf3af19abf0d527acf4f490cc4fb2f32b6
unknown <jasminesony759@gmail.com>
2024-08-12 17:45:26 +0530

    second

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -2,7 +2,7 @@ import react from 'react'
 
 const Home = () =>{
     return (
-        <div>welcome</div>
+        <div><img src="C:\Users\DELL\OneDrive\Pictures\pure-nature-landscape-single-tree-in-green-field-free-photo.webp"></img></div>
     )
 }
 export default Home ;
